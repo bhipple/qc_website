df6d5f04ccad8adc2edf1f4c9aa9e6da66188aa0
Benjamin Hipple <brh2113@columbia.edu>
2015-10-13 19:51:12 -0400

    Got something up and running, although it isn't pretty

---
+++ README.md
@@ -0,0 +1,2 @@
+# hunchentoot
+Getting a webserver up that runs Steel Bank Common Lisp code.  Neat!
--- server.lisp
+++ server.lisp
@@ -1,13 +1,3 @@
-(ql:quickload "hunchentoot")
-
-(hunchentoot:define-easy-handler (say-yo :uri "/yo") (name)
-                                 (setf (hunchentoot:content-type*) "text/plain")
-                                 (format nil "Hey~@[ ~A~]!" name))
-
-(hunchentoot:define-easy-handler (arbitrary :uri "/t") ()
-                                 (setf (hunchentoot:content-type*) "text/plain")
-                                 (princ "Hey Ben"))
-
 ; Reads a file and returns the lines as a list
 (defun get-file (file)
   (with-open-file (stream file)
@@ -15,5 +5,25 @@
           while line
           collect line)))
 
-(defparameter acceptor (make-instance 'hunchentoot:easy-acceptor :port 4242))
-(hunchentoot:start acceptor)
+(defun print-line (line)
+    (format nil "Line = ~a~&" line))
+
+(defun print-lines (lines)
+  (mapcar #'print-line lines))
+
+
+;; ============================================================================
+;;                           Hunchentoot Handlers
+;; ============================================================================
+(ql:quickload "hunchentoot")
+
+(hunchentoot:define-easy-handler (say-yo :uri "/yo") (name)
+                                 (setf (hunchentoot:content-type*) "text/plain")
+                                 (format nil "Hey~@[ ~A~]!" name))
+
+(hunchentoot:define-easy-handler (tickets :uri "/t") ()
+                                 (setf (hunchentoot:content-type*) "text/plain")
+                                 (format nil "~A" (print-lines (get-file "tickets.csv"))))
+
+;(defparameter acceptor (make-instance 'hunchentoot:easy-acceptor :port 4242))
+;(hunchentoot:start acceptor)
