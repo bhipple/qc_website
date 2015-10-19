6b79ba3606c24b0b030bdb47beaacbfcbda693de
Benjamin Hipple <brh2113@columbia.edu>
2015-10-18 23:57:21 -0400

    Initial working server with generated code output

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # hunchentoot
 Getting a webserver up that runs Steel Bank Common Lisp code.  Neat!
+
+## Startup
+Just compile and run server.lisp
--- server.lisp
+++ server.lisp
@@ -1,29 +1,40 @@
 ; Reads a file and returns the lines as a list
+
+(hunchentoot:define-easy-handler (say-yo :uri "/yo") (name)
+  (setf (hunchentoot:content-type*) "text/plain")
+  (format nil "Hey~@[ ~A~]!" name))
+
+(hunchentoot:define-easy-handler (tickets :uri "/t") ()
+  (setf (hunchentoot:content-type*) "text/plain")
+  (handle-tickets))
+
 (defun get-file (file)
   (with-open-file (stream file)
     (loop for line = (read-line stream nil)
           while line
           collect line)))
 
-(defun print-line (line)
-    (format nil "Line = ~a~&" line))
+(defun get-fname-content-pair (fname)
+  (cons (file-namestring fname) (get-file fname)))
+
+(defun get-csv-files (path)
+  (directory (concatenate 'string path "*.csv")))
 
 (defun print-lines (lines)
-  (mapcar #'print-line lines))
+  (format nil "~{~a~&~}" lines))
 
+(defun handle-tickets ()
+  (let* ((filenames (get-csv-files "./"))
+         (name-content-pairs (mapcar
+                               #'get-fname-content-pair
+                               filenames)))
+    (loop for f in name-content-pairs
+          collect (format nil "~a:~%~a~&~%" (car f) (print-lines (cdr f))))))
 
 ;; ============================================================================
 ;;                           Hunchentoot Handlers
 ;; ============================================================================
 (ql:quickload "hunchentoot")
-
-(hunchentoot:define-easy-handler (say-yo :uri "/yo") (name)
-                                 (setf (hunchentoot:content-type*) "text/plain")
-                                 (format nil "Hey~@[ ~A~]!" name))
-
-(hunchentoot:define-easy-handler (tickets :uri "/t") ()
-                                 (setf (hunchentoot:content-type*) "text/plain")
-                                 (format nil "~A" (print-lines (get-file "tickets.csv"))))
 
 ;(defparameter acceptor (make-instance 'hunchentoot:easy-acceptor :port 4242))
 ;(hunchentoot:start acceptor)
