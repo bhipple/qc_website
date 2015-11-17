d8e84939fe7162240ffe1e7cc0376f2c68be55f6
Benjamin Hipple <bhipple@bloomberg.net>
2015-11-17 22:42:01 +0000

    Revert "HTML revert?"
    This reverts commit b762423b1324919a7e3b207e3f8b550132b826f4.

--- html.lisp
+++ html.lisp
@@ -17,13 +17,14 @@
                  ,no))))
 
 (defmacro tag (name atts &body body)
-  `(progn (print-tag ',name
-                     (list ,@(mapcar (lambda (x)
-                                       `(cons ',(car x) ,(cdr x)))
-                                     (pairs atts)))
-                     nil)
-          ,@body
-          (print-tag ',name nil t)))
+  `(concatenate 'string
+               (print-tag ',name
+                          (list ,@(mapcar (lambda (x)
+                                            `(cons ',(car x) ,(cdr x)))
+                                          (pairs atts)))
+                          nil)
+               ,@body
+               (print-tag ',name nil t)))
 
 (defmacro html (&body body)
   `(tag html ()
@@ -46,11 +47,12 @@
     (f lst nil)))
 
 (defun print-tag (name alst closingp)
-  (princ #\<)
-  (when closingp
-    (princ #\/))
-  (princ (string-downcase name))
-  (mapc (lambda (att)
-          (format t " ~a=\"~a\"" (string-downcase (car att)) (cdr att)))
-        alst)
-  (princ #\>))
+  (concatenate 'string
+               "<"
+               (when closingp
+                 "/")
+               (string-downcase name)
+               (mapc (lambda (att)
+                       (format nil " ~a=\"~a\"" (string-downcase (car att)) (cdr att)))
+                     alst)
+               ">"))
